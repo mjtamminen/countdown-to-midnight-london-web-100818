8b16c770ed3e9a3ae1dfda3d5ac21b755815f0d7
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-30 13:45:35 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -12,8 +12,9 @@ end
 def countdown_with_sleep(integer)
   x = integer
   while x > 0
-  puts "#{x} SECOND(S)!" sleep(1)
+  puts "#{x} SECOND(S)!"
   x -= 1
+  sleep(1)
 end
   return "HAPPY NEW YEAR!"
 end
